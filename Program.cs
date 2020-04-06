e8dc6750590ad5fcf5801fb19ee89b39963b3237
unknown <nkosimova06@gmail.com>
2020-04-06 21:38:57 +0500

    class Employee with fields and constuctor

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public class Employee
+        {
+        public string EmplName{get; set;}
+        public string EmplSurname{get; set;}
+        public string EmplPost{get; set;}
+        public int EmplExperience{get; set;}
+
+        public Employee(string surname, string name)
+        {
+            EmplSurname = surname;
+            EmplName = name;
+        }
+
+        }
+    }
+}
